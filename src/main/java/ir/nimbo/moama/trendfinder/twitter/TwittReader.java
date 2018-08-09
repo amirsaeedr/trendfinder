baf439b50b206c1e8bb57e3e423a422db7cb7d78
amirsaeedr <amirsaeedranjbar@gmail.com>
2018-08-09 13:35:43 +0430

    modify twitter reader class

--- src/main/java/ir/nimbo/moama/trendfinder/twitter/TwittReader.java
+++ src/main/java/ir/nimbo/moama/trendfinder/twitter/TwittReader.java
@@ -6,24 +6,20 @@ import ir.nimbo.moama.trendfinder.util.ConfigManager;
 import ir.nimbo.moama.trendfinder.util.PropertyType;
 
 public class TwittReader {
-    private ConfigManager configManager;
-    private String appKey;
     private String channel;
-    private String endPoint;
-    private RtmClientAdapter twitterAdapter;
     private SubscriptionAdapter twitterSubscriber;
     private RtmClient client;
     public TwittReader() {
-        configManager = ConfigManager.getInstance();
-        appKey = configManager.getProperty(PropertyType.TWITTER_APP_KEY);
+        ConfigManager configManager = ConfigManager.getInstance();
+        String appKey = configManager.getProperty(PropertyType.TWITTER_APP_KEY);
         channel = configManager.getProperty(PropertyType.TWITTER_CHANNEL);
-        endPoint = configManager.getProperty(PropertyType.TWITTER_END_POINT);
-        twitterAdapter = new TwitterClientAdapter();
+        String endPoint = configManager.getProperty(PropertyType.TWITTER_END_POINT);
+        RtmClientAdapter twitterAdapter = new TwitterClientAdapter();
         twitterSubscriber = new SubscriptionAdapter();
-        client = new RtmClientBuilder(configManager.getProperty(PropertyType.TWITTER_END_POINT), configManager.getProperty(PropertyType.TWITTER_APP_KEY)).setListener(twitterAdapter).build();
+        client = new RtmClientBuilder(endPoint, appKey).setListener(twitterAdapter).build();
     }
     public void subscribe(){
-        client.createSubscription(configManager.getProperty(PropertyType.TWITTER_CHANNEL), SubscriptionMode.SIMPLE ,twitterSubscriber);
+        client.createSubscription(channel, SubscriptionMode.SIMPLE ,twitterSubscriber);
         client.start();
     }
 }
