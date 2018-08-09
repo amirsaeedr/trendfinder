f6df2cbea70c07a1268f02bbe00ea0188f4eb22f
amirsaeedr <amirsaeedranjbar@gmail.com>
2018-08-09 10:01:08 +0430

    create project

---
+++ src/main/java/ir/nimbo/moama/trendfinder/App.java
@@ -0,0 +1,13 @@
+package ir.nimbo.moama.trendfinder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
